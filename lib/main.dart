0f8083992ed2f492d4d791b32124e9bc1850e9e6
sotogumi <u961841f@ecs.osaka-u.ac.jp>
2025-03-26 09:50:09 +0900

    providerの導入

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_jr_hackathon/firebase_options.dart';
 import 'package:flutter_jr_hackathon/home/home_page.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
