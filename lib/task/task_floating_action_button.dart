c8a4def6ba989a33a9ee816c67f8516d289d1f4f
tkosei2003 <tkosei2003@gmail.com>
2025-03-26 09:02:22 +0900

    task追加の入力画面作成

--- lib/task/task_floating_action_button.dart
+++ lib/task/task_floating_action_button.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_jr_hackathon/sheet/task_sheet.dart';
 
 class TaskFloatingActionButton extends StatelessWidget {
   const TaskFloatingActionButton({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return FloatingActionButton(onPressed: () {});
+    return FloatingActionButton(onPressed: () => showModalBottomSheet(
+          isScrollControlled: true,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+          context: context,
+          builder: (context) => AddNewTask()),
+      child: Icon(Icons.add),
+    );
   }
 }
