1128c9b5453b9c050ba7082fd7fe9d0687be6438
tkosei2003 <tkosei2003@gmail.com>
2025-03-27 22:29:08 +0900

    Revert "Merge branch 'main' of https://github.com/r-sei/Flutter_Jr_hackathon"
    This reverts commit a298cf95ffb6e315f9d5f123b06d5d20418903aa, reversing
    changes made to 78df9164358366b4014fb2f6e5883a69398b01b9.

--- lib/task/task_page.dart
+++ lib/task/task_page.dart
@@ -1,54 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_jr_hackathon/task/task_floating_action_button.dart';
-import 'package:flutter_jr_hackathon/task/task_list_widget.dart';
 import 'package:flutter_jr_hackathon/widget/appbar.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:gap/gap.dart';
-import 'package:intl/intl.dart';
 
 class TaskPage extends ConsumerWidget {
   const TaskPage({
     super.key,
   });
 
-  String getWeekRange() {
-    DateTime now = DateTime.now();
-    int currentWeekday = now.weekday;
-
-    DateTime monday = now.subtract(Duration(days: currentWeekday - 1));
-    DateTime sunday = now.add(Duration(days: 7 - currentWeekday));
-
-    String formattedMonday = DateFormat('MM/dd').format(monday);
-    String formattedSunday = DateFormat('MM/dd').format(sunday);
-    
-    return '$formattedMonday - $formattedSunday';
-  }
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30),
-          child: Column(children: [
-            const Gap(20),
-            Text(
-              getWeekRange(),
-              style: const TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-              ),
-            const Gap(20),
-            ListView.builder(
-              itemCount: 5,
-              shrinkWrap: true,
-              itemBuilder: (context, index) =>
-                const TaskListWidget(),
-            )
-          ],),
-          ),
-      ),
       appBar: MyAppBar(),
       floatingActionButton: const TaskFloatingActionButton(),
     );
