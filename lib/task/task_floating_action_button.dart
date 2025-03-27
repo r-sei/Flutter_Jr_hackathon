c6fdbd6f0116bf2b85e0e2cb6444e7bd1f81145d
tkosei2003 <tkosei2003@gmail.com>
2025-03-27 15:32:58 +0900

    floating buttonのデザインをappbarと統一（コンフリクトおきたらごめん）

--- lib/task/task_floating_action_button.dart
+++ lib/task/task_floating_action_button.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_jr_hackathon/sheet/task_sheet.dart';
+import 'package:flutter_jr_hackathon/style/color.dart';
 
 class TaskFloatingActionButton extends StatelessWidget {
   const TaskFloatingActionButton({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return FloatingActionButton(onPressed: () => showModalBottomSheet(
+    return FloatingActionButton(
+      backgroundColor: col['accent1'],
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+      onPressed: () => showModalBottomSheet(
           isScrollControlled: true,
           shape:
               RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
           context: context,
           builder: (context) => AddNewTask()),
-      child: Icon(Icons.add),
+      child: Icon(Icons.add, color: col['text1']),
     );
   }
 }
