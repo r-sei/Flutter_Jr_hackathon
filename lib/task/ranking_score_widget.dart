3a8c66cdc5942b7e97628552c20ad2f563fd2860
YusukeWasaka <sfb01162@st.osakafu-u.ac.jp>
2025-03-28 22:25:21 +0900

    スコアを計算してランキングに反映

--- lib/task/ranking_score_widget.dart
+++ lib/task/ranking_score_widget.dart
@@ -5,20 +5,41 @@ class RankingScoreWidget extends StatelessWidget {
   const RankingScoreWidget({
     super.key,
     required this.rank,
+    required this.taskScores,
   });
 
   final int rank;
+  final Map<String, double> taskScores;
 
   @override
   Widget build(BuildContext context) {
-    return Row(children: [
-      const Gap(50),
-      Expanded(
-        child: Text('$rank位: 名前')
-        ),
-      // Gap(20),
-      Text('達成度'),
-      const Gap(100),
-    ],);
+
+    print(taskScores.entries);
+    var sortedEntries = taskScores.entries.toList()
+      ..sort((a, b) => b.value.compareTo(a.value)); // 降順でソート
+
+    // rank番目のエントリを取得（rankは1始まりであることを想定）
+    if (rank - 1 >= sortedEntries.length) {
+      return Text('ランキング外');
+    }
+
+    var rankEntry = sortedEntries[rank-1];
+
+    double displayValue = rankEntry.value.isNaN ? 0 : rankEntry.value;
+
+    return Column(
+      children: [
+        Row(children: [
+          Gap(50),
+          Expanded(
+            child: Text('$rank位: ' + rankEntry.key),
+            ),
+          // Gap(20),
+          Text(displayValue.toString()),
+          const Gap(100),
+        ],),
+        Gap(10),
+      ],
+    );
   }
 }
