9c64637b333d0d51302af92e22c6b12cc44f4792
r-sei <ryousei3964@gmail.com>
2025-03-29 10:10:29 +0900

    ⚡️ UIの微調整

--- lib/home/components/test_task_progress_post.dart
+++ lib/home/components/test_task_progress_post.dart
@@ -1,17 +1,70 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_jr_hackathon/home/components/good_button.dart';
 import 'package:flutter_jr_hackathon/models/progress.dart';
+import 'package:flutter_jr_hackathon/models/task.dart';
+import 'package:flutter_jr_hackathon/task/task_view_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gap/gap.dart';
 import 'package:intl/intl.dart';
 import 'package:percent_indicator/linear_percent_indicator.dart';
 
-class TestTaskProgressPost extends StatelessWidget {
-  const TestTaskProgressPost({super.key, required this.progress});
+class TestTaskProgressPost extends ConsumerWidget {
+  TestTaskProgressPost({super.key, required this.progress});
 
   final Progress progress;
 
+  final oneWeekAgo = DateTime.now().subtract(const Duration(days: 7));
+  //todo: 現在選択されているアカウントに応じたtileのUIに変更（いいねボタンとか特に）
+
+  String getWeekMondayToSunday(DateTime dateTime) {
+    // 入力された日付がその週の何曜日かを取得
+    int weekday = dateTime.weekday;
+
+    // 月曜日の日付を計算 (weekday が 1 は月曜日)
+    DateTime monday = dateTime.subtract(Duration(days: weekday - 1));
+
+    // 日曜日の日付を計算 (weekday が 7 は日曜日)
+    DateTime sunday = dateTime.add(Duration(days: 7 - weekday));
+
+    // 月曜日と日曜日の日付を yyyy/MM/DD 形式でフォーマット
+    String formattedMonday = DateFormat('yyyy/MM/dd').format(monday);
+    String formattedSunday = DateFormat('yyyy/MM/dd').format(sunday);
+
+    // 結果を "yyyy/MM/DD - yyyy/MM/DD" 形式で返す
+    return '$formattedMonday - $formattedSunday';
+  }
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final List<Task> taskList = ref.watch(tasksProvider).when(
+      data: (data) {
+        List<Task> tasks =
+            data.docs.map((e) => Task.fromJson(e.data())).toList();
+
+        // createdAt で降順ソート (新しい順)
+        tasks.sort((a, b) => b.createdAt.compareTo(a.createdAt));
+
+        // createdAt で昇順ソート (古い順)
+        // tasks.sort((a, b) => a.createdAt.compareTo(b.createdAt));
+
+        return tasks;
+      },
+      error: (error, _) {
+        return [];
+      },
+      loading: () {
+        return [];
+      },
+    );
+
+    DateTime taskCreatedDate =
+        DateTime.now(); // エラー回避でDateTime.now()で初期化しているけど、ifで必ずヒットするから問題ないはず...
+    for (final task in taskList) {
+      if (task.taskTitle == progress.taskTitle) {
+        taskCreatedDate = task.createdAt;
+      }
+    }
+
     return Container(
       padding: const EdgeInsets.all(12),
       decoration: BoxDecoration(
@@ -25,27 +78,49 @@ class TestTaskProgressPost extends StatelessWidget {
           // ヘッダー部分（アイコン + ユーザー情報）
           Row(
             children: [
-              CircleAvatar(
-                radius: 24,
-                backgroundColor: Colors.grey.shade200,
-                child: Text(progress.userName[0]),
-              ),
-              const Gap(8),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    progress.userName,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
+              Expanded(
+                child: Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 24,
+                      backgroundColor: Colors.grey.shade200,
+                      child: Text(progress.userName[0]),
                     ),
+                    const Gap(8),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          progress.userName,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                        Text(
+                          DateFormat('yyyy/MM/dd').format(progress.createdAt),
+                          style: TextStyle(
+                              fontSize: 12, color: Colors.grey.shade600),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              const Gap(16),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 8, 8, 0),
+                child: Text(
+                  '期限切れ',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: getWeekMondayToSunday(taskCreatedDate) ==
+                            getWeekMondayToSunday(DateTime.now())
+                        ? Colors.white
+                        : Colors.red,
                   ),
-                  Text(
-                    DateFormat('yyyy/MM/dd').format(progress.createdAt),
-                    style: TextStyle(fontSize: 12, color: Colors.grey.shade600),
-                  ),
-                ],
+                ),
               ),
             ],
           ),
@@ -84,21 +159,20 @@ class TestTaskProgressPost extends StatelessWidget {
               const Gap(24),
               const Icon(Icons.directions_run, size: 18),
               const Gap(4),
-              Expanded(
-                child: Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey, width: 1),
-                    borderRadius: BorderRadius.circular(4),
-                  ),
-                  child: LinearPercentIndicator(
-                    width: 140.0,
-                    lineHeight: 14.0,
-                    percent: progress.achieveLevel,
-                    backgroundColor: Colors.grey.shade200,
-                    progressColor: Colors.green,
-                    barRadius: Radius.zero,
-                    padding: EdgeInsets.zero,
-                  ),
+              Container(
+                width: 202.0,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey, width: 1),
+                  borderRadius: BorderRadius.circular(4),
+                ),
+                child: LinearPercentIndicator(
+                  width: 200.0,
+                  lineHeight: 14.0,
+                  percent: progress.achieveLevel,
+                  backgroundColor: Colors.grey.shade200,
+                  progressColor: Colors.green,
+                  barRadius: Radius.zero,
+                  padding: EdgeInsets.zero,
                 ),
               ),
               const Gap(8),
